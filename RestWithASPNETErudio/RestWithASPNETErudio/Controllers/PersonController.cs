d61bc355219bae772361a07572f37fb292be6419
Pampolini <mateuspcouto@gmail.com>
2025-06-26 19:20:00 -0300

    Implementacao ate a conexao com o DB

--- RestWithASPNETErudio/RestWithASPNETErudio/Controllers/PersonController.cs
+++ RestWithASPNETErudio/RestWithASPNETErudio/Controllers/PersonController.cs
@@ -1,23 +1,58 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETErudio.Model;
+using RestWithASPNETErudio.Services;
 
 namespace RestWithASPNETErudio.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
 
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            return null;
+            return Ok(_personService.FindAll());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);            
+            return NoContent();
+        }
+
     }
 }
--- RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
+++ RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETErudio.Model.Context;
+using RestWithASPNETErudio.Services;
+using RestWithASPNETErudio.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection,new MySqlServerVersion(new Version(8, 0, 42))));
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
